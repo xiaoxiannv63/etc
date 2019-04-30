1f892613d5203e2ca813e2d80a922e3db3f77104
ZM <jane@ZM.local>
2019-04-30 16:53:13 +0800

    样式

--- .tea/entryFiles-production/index$.worker.js
+++ .tea/entryFiles-production/index$.worker.js
@@ -1,21 +1,6 @@
-if(!self.__appxInited) {
-self.__appxInited = 1;
-
 
 require('./config$');
 require('./importScripts$');
-
-var AFAppX = self.AFAppX;
-self.getCurrentPages = AFAppX.getCurrentPages;
-self.getApp = AFAppX.getApp;
-self.Page = AFAppX.Page;
-self.App = AFAppX.App;
-self.my = AFAppX.bridge || AFAppX.abridge;
-self.abridge = self.my;
-self.Component = AFAppX.WorkerComponent || function(){};
-self.$global = AFAppX.$global;
-
-
 function success() {
 require('../..//app');
 require('../../node_modules/mini-antui/es/flex/index');
@@ -71,4 +56,3 @@ require('../../pages/routeInvoice/routeInvoice');
 require('../../pages/redChangeStatus/redChangeStatus');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
-}
